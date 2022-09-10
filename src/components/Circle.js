1a84828ba5fa6de400592ebec2e931f5cce31b70
Shakir Ali <shakiralihamza05@gmail.com>
2022-09-11 00:05:18 +0500

    App Ready

---
+++ src/components/Circle.js
@@ -0,0 +1,265 @@
+/* eslint-disable no-unused-vars,no-eval */
+// noinspection ES6CheckImport,JSUnusedLocalSymbols,JSValidateTypes
+
+import React from 'react';
+import './Circle.css';
+import useWebAnimations, {fadeIn} from "@wellyshen/use-web-animations";
+import {Box, Grid, Typography} from "@mui/material";
+import animals from "./animals";
+
+
+const Circle = () => {
+    const [selected, setSelected] = React.useState(0);
+    const [hovered, setHovered] = React.useState(0);
+    const [showingAnimalDetails, setShowingAnimalDetails] = React.useState(false);
+    const [animalToShow, setAnimalToShow] = React.useState(null);
+
+    //below is some reusable content
+
+    const numberOfElements = 19;
+    const translateLength = 230;
+    const angle = 360 / numberOfElements;
+    const keyframes = (i) => {
+        return [{transform: `rotate(${i * angle}deg) translate(${translateLength}px)`}, {transform: `rotate(${i * angle + 360}deg) translate(${translateLength}px)`},];
+    }
+    const animalContentAnimationOptionsIn = {
+        ...fadeIn, animationOptions: {
+            duration: 500, easing: "ease-in-out", fill: "forwards"
+
+        }
+    }
+    const animationOptions = {
+        delay: 0, duration: 80000, iterations: 1000,
+    }
+    const scratchyOptions = {
+        keyframes: [{backgroundPosition: '0 0'}, {backgroundPosition: '0 0'}, {backgroundPosition: '20px -20px'}, {backgroundPosition: '20px -20px'}, {backgroundPosition: '40px -40px'}, {backgroundPosition: '40px -40px'}, {backgroundPosition: '60px -60px'}, {backgroundPosition: '60px -60px'}, {backgroundPosition: '0 0'},],
+        animationOptions: {duration: 250, iterations: 10000, easing: "steps(8, end)"}
+    }
+
+    // below are the functions that handle user interaction
+
+    const handleMouseOver = (i) => {
+        if (!showingAnimalDetails) {
+            setHovered(i + 1);
+            setAnimalToShow(animals[i]);
+            animateAnimalContentIn();
+        }
+    }
+    const handleElementClick = (index) => {
+        setSelected(index + 1);
+        hideElements();
+        setHovered(0)
+        showAnimalContent();
+        setShowingAnimalDetails(true);
+    }
+    const handleBack = () => {
+        bringBackElements()
+        rotateElements()
+        hideAnimalContent()
+        setTimeout(() => {
+            setShowingAnimalDetails(false);
+        }, 1000)
+    }
+
+    // below are the functions that handle the animations
+
+    const showAnimalContent = () => {
+        animalContent.animate({
+            keyframes: [{transform: "translateY(0px)"}, {transform: "translateX(-280px)"}], animationOptions: {
+                duration: 1000, delay: 500, easing: "cubic-bezier(0.275, 0.82, 0.165, 1)", fill: "forwards"
+            }
+        });
+
+        animalDetail.animate({
+            keyframes: [{transform: "translateX(400px)"}, {transform: "translateX(140px)", opacity: 1}],
+            animationOptions: {
+                duration: 1000, delay: 600, easing: "cubic-bezier(0.275, 0.82, 0.165, 1)", fill: "forwards"
+            }
+        });
+        closeButton.animate({
+            keyframes: [{opacity: 0},{opacity: 1}],
+            animationOptions: {
+                duration: 1000, delay: 700, easing: "cubic-bezier(0.275, 0.82, 0.165, 1)", fill: "forwards"
+            }
+        });
+    }
+    const hideAnimalContent = () => {
+        animalContent.animate({
+            keyframes: [{transform: "translateX(-280px)"}, {transform: "translateY(0px)"}], animationOptions: {
+                duration: 1000, delay: 500, easing: "cubic-bezier(0.275, 0.82, 0.165, 1)", fill: "forwards"
+            }
+        });
+
+        animalDetail.animate({
+            keyframes: [{transform: "translateX(140px)"}, {opacity: 0}], animationOptions: {
+                duration: 400, delay: 500, easing: "cubic-bezier(0.275, 0.82, 0.165, 1)", fill: "forwards"
+            }
+        });
+        closeButton.animate({
+            keyframes: [{opacity: 1},{opacity: 0}],
+            animationOptions: {
+                duration: 500, delay: 200, easing: "cubic-bezier(0.275, 0.82, 0.165, 1)", fill: "forwards"
+            }
+        });
+    }
+    const hideElements = () => {
+        for (let i = 0; i < numberOfElements; i++) {
+            eval(`element${i + 1}.animate({
+                keyframes: [{opacity: 1}, {
+                    transform: \`rotate(${i * angle}deg) translateX(${translateLength}px)\`, opacity: 0.2
+                }, {transform: \`rotate(${i * angle}deg) translateX(${translateLength + 600}px)\`, opacity: 0}],
+                animationOptions: {duration: 1000, easing: "ease-in", fill: "forwards"}
+            })`);
+        }
+    }
+    const bringBackElements = () => {
+        for (let i = 0; i < numberOfElements; i++) {
+            eval(`element${i + 1}.animate({
+                keyframes: [{transform: \`rotate(${i * angle}deg) translate(${translateLength}px)\`, opacity: 1}],
+                animationOptions: {duration: 1000, easing: "ease-in-out", fill: "forwards"}
+            })`);
+        }
+    }
+    const rotateElements = () => {
+        for (let i = 0; i < numberOfElements; i++) {
+            eval(`element${i + 1}.animate({
+                keyframes: keyframes(i + 1), animationOptions: {...animationOptions, delay: 1000}
+            })`);
+        }
+    }
+    const animateAnimalContentIn = () => {
+        animalType.animate({...animalContentAnimationOptionsIn});
+        animalImage.animate({...animalContentAnimationOptionsIn});
+        animalName.animate({...animalContentAnimationOptionsIn});
+        pieceNumber.animate({
+            keyframes: [{marginTop: "20px", opacity: 0}, {marginTop: "0px", opacity: 1},],
+            animationOptions: {duration: 500, delay: 0, fill: "forwards", easing: "ease-in-out"}
+        });
+        animalNameScratchy.animate({...scratchyOptions});
+    }
+
+    // below are the references to the elements that will be animated
+    const element1 = useWebAnimations({keyframes: keyframes(1), animationOptions: {...animationOptions}});
+    const element2 = useWebAnimations({keyframes: keyframes(2), animationOptions: {...animationOptions}});
+    const element3 = useWebAnimations({keyframes: keyframes(3), animationOptions: {...animationOptions}});
+    const element4 = useWebAnimations({keyframes: keyframes(4), animationOptions: {...animationOptions}});
+    const element5 = useWebAnimations({keyframes: keyframes(5), animationOptions: {...animationOptions}});
+    const element6 = useWebAnimations({keyframes: keyframes(6), animationOptions: {...animationOptions}});
+    const element7 = useWebAnimations({keyframes: keyframes(7), animationOptions: {...animationOptions}});
+    const element8 = useWebAnimations({keyframes: keyframes(8), animationOptions: {...animationOptions}});
+    const element9 = useWebAnimations({keyframes: keyframes(9), animationOptions: {...animationOptions}});
+    const element10 = useWebAnimations({keyframes: keyframes(10), animationOptions: {...animationOptions}});
+    const element11 = useWebAnimations({keyframes: keyframes(11), animationOptions: {...animationOptions}});
+    const element12 = useWebAnimations({keyframes: keyframes(12), animationOptions: {...animationOptions}});
+    const element13 = useWebAnimations({keyframes: keyframes(13), animationOptions: {...animationOptions}});
+    const element14 = useWebAnimations({keyframes: keyframes(14), animationOptions: {...animationOptions}});
+    const element15 = useWebAnimations({keyframes: keyframes(15), animationOptions: {...animationOptions}});
+    const element16 = useWebAnimations({keyframes: keyframes(16), animationOptions: {...animationOptions}});
+    const element17 = useWebAnimations({keyframes: keyframes(17), animationOptions: {...animationOptions}});
+    const element18 = useWebAnimations({keyframes: keyframes(18), animationOptions: {...animationOptions}});
+    const element19 = useWebAnimations({keyframes: keyframes(19), animationOptions: {...animationOptions}});
+
+    const origamiHeadingBox = useWebAnimations({
+        keyframes: [{marginTop: "20px"}, {marginTop: "0px"}],
+        animationOptions: {duration: 500, delay: 200, fill: "forwards", easing: "ease-in-out"}
+    });
+    const origamiMainBox = useWebAnimations({
+        keyframes: [{marginTop: "20px"}, {marginTop: "0px"}],
+        animationOptions: {duration: 500, delay: 0, fill: "forwards", easing: "ease-in-out"}
+    });
+    const origamiMain = useWebAnimations({...scratchyOptions});
+
+    const origamiText = useWebAnimations({...fadeIn, animationOptions: {duration: 500, delay: 500, fill: "forwards"}});
+    const selectPiece = useWebAnimations({...fadeIn, animationOptions: {duration: 500, delay: 1000, fill: "forwards"}});
+
+    const animalImage = useWebAnimations()
+    const animalType = useWebAnimations()
+    const animalName = useWebAnimations()
+    const pieceNumber = useWebAnimations()
+    const animalDetail = useWebAnimations()
+
+    const animalContent = useWebAnimations()
+    const animalNameScratchy = useWebAnimations()
+    const closeButton = useWebAnimations()
+
+    return (
+        <Grid container sx={{height: '100vh', width: '100%'}} justifyContent={'center'} alignItems={'center'}>
+            <Grid item xs={'auto'}>
+                {Array.from({length: numberOfElements}, (v, i) => {
+                    const element = `element${i + 1}`;
+                    return (<div
+                        className={`element ${hovered === i + 1 && 'selected'}`}
+                        style={{zIndex: i + 1}}
+                        onClick={() => handleElementClick(i)}
+                        onMouseOver={() => {
+                            handleMouseOver(i)
+                        }}
+                        ref={eval(element).ref}
+                    >
+                        <div className={'dot'}/>
+                    </div>)
+                })}
+            </Grid>
+            {hovered === 0 && selected === 0 ? (
+                <Grid item xs={'auto'} sx={{position: 'absolute', textAlign: 'center'}}>
+                    <Box ref={origamiHeadingBox.ref} sx={{marginTop: '20px'}}>
+                        <Typography className={'origami-heading'}>
+                            In Origami
+                        </Typography>
+                    </Box>
+                    <Box ref={origamiMainBox.ref} sx={{marginTop: '20px'}}>
+                        <Typography className={'origami'} variant={'h2'} ref={origamiMain.ref}>
+                            {numberOfElements} Animals
+                        </Typography>
+                    </Box>
+                    <Box ref={origamiText.ref} sx={{opacity: 0}}>
+                        <Typography className={'text'} variant={'body2'}>
+                            Made with origami
+                        </Typography>
+                    </Box>
+                    <Box ref={selectPiece.ref} sx={{opacity: 0}}>
+                        <Typography className={'select-piece'} variant={'body2'}
+                                    sx={{fontSize: 12}}
+                        >
+                            Select a piece from the ring
+                        </Typography>
+                    </Box>
+                </Grid>
+            ) : (
+                <>
+                    <Grid item xs={'auto'} ref={animalContent.ref}
+                          sx={{position: 'absolute', textAlign: 'center', mt: -5}}>
+                        <Box ref={animalImage.ref}>
+                            <img src={animalToShow.image} alt={animalToShow.name}
+                                 style={{height: '200px'}}/>
+                        </Box>
+                        <Box ref={pieceNumber.ref}>
+                            <Typography className={'piece-number'}>
+                                Piece {animalToShow.id}
+                            </Typography>
+                        </Box>
+                        <Box ref={animalName.ref}>
+                            <Typography className={'animal-name'} ref={animalNameScratchy.ref} variant={'h3'}>
+                                {animalToShow.name}
+                            </Typography>
+                        </Box>
+                        <Box sx={{marginTop: '10px'}} ref={animalType.ref}>
+                            <Typography className={'animal-type'} variant={'body1'}>
+                                {animalToShow.type}
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={'auto'} sx={{position: 'absolute', textAlign: 'center', opacity: 0}}
+                          ref={animalDetail.ref}>
+                        <Typography className={'animal-detail'}>
+                            {animalToShow.details}
+                        </Typography>
+                    </Grid>
+                    <Box className={'close-button'} onClick={handleBack} ref={closeButton.ref}/>
+                </>
+            )}
+        </Grid>
+    )
+};
+
+export default Circle;
---
+++ src/components/animals.js
@@ -0,0 +1,158 @@
+import Frog from "./images/frog.png";
+import Butterfly from "./images/butterfly.png";
+import Camel from "./images/camel.png";
+import Caterpillar from "./images/caterpillar.png";
+import Cow from "./images/cow.png";
+import Elephant from "./images/elephant.png";
+import Fox from "./images/fox.png";
+import Giraffe from "./images/giraffe.png";
+import Horse from "./images/horse.png";
+import Koala from "./images/koala.png";
+import Lion from "./images/lion.png";
+import Lobster from "./images/lobster.png";
+import PolarBear from "./images/polar-bear.png";
+import Rabbit from "./images/rabbit.png";
+import Reindeer from "./images/reindeer.png";
+import Rhino from "./images/rhino.png";
+import Shark from "./images/shark.png";
+import Whale from "./images/whale.png";
+import Zebra from "./images/zebra.png";
+
+const animals = [
+    {
+        id: 1,
+        name: 'Frog',
+        image: Frog,
+        type: 'Carnivorous',
+        details: 'A frog is any member of a diverse and largely carnivorous group of short-bodied, tailless amphibians composing the order Anura'
+    },
+    {
+        id: 2,
+        name: 'Butterfly',
+        image: Butterfly,
+        type: 'macrolepidopteran clade Rhopalocera',
+        details: 'Butterflies are insects in the macrolepidopteran clade Rhopalocera from the order Lepidoptera, which also includes moths. Adult butterflies have large, often brightly coloured wings, and conspicuous, fluttering flight'
+    },
+    {
+        id: 3,
+        name: 'Camel',
+        image: Camel,
+        type: 'even-toed ungulate',
+        details: 'A camel is an even-toed ungulate in the genus Camelus that bears distinctive fatty deposits known as "humps" on its back. Camels have long been domesticated and, as livestock, they provide food and textiles.'
+    },
+    {
+        id: 4,
+        name: 'Caterpillar',
+        image: Caterpillar,
+        type: 'larva',
+        details: 'Caterpillars are the larval stage of members of the order Lepidoptera. As with most common names, the application of the word is arbitrary, since the larvae of sawflies are commonly called caterpillars as well. Both lepidopteran and symphytan larvae have eruciform body shapes.'
+    },
+    {
+        id: 5,
+        name: 'Cow',
+        image: Cow,
+        type: 'cloven-hooved, herbivores',
+        details: 'Cattle are large, domesticated, cloven-hooved, herbivores. They are a prominent modern member of the subfamily Bovinae and the most widespread species of the genus Bos. Adult females are referred to as cows and adult males are referred to as bulls.'
+    },
+    {
+        id: 6,
+        name: 'Elephant',
+        image: Elephant,
+        type: 'land animals',
+        details: 'Elephants are the largest existing land animals. Three living species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant. They are the only surviving members of the family Elephantidae and the order Proboscidea.'
+    },
+    {
+        id: 7,
+        name: 'Fox',
+        image: Fox,
+        type: 'omnivorous',
+        details: 'Foxes are small to medium-sized, omnivorous mammals belonging to several genera of the family Canidae. They have a flattened skull, upright triangular ears, a pointed, slightly upturned snout, and a long bushy tail. Twelve species belong to the monophyletic "true foxes" group of genus Vulpes.'
+    },
+    {
+        id: 8,
+        name: 'Giraffe',
+        image: Giraffe,
+        type: 'hoofed mammal',
+        details: 'The giraffe is a large African hoofed mammal belonging to the genus Giraffa. It is the tallest living terrestrial animal and the largest ruminant on Earth. Traditionally, giraffes were thought to be one species, Giraffa camelopardalis, with nine subspecies.'
+    },
+    {
+        id: 9,
+        name: 'Horse',
+        image: Horse,
+        type: 'A domesticated animal',
+        details: 'The horse is a domesticated, one-toed, hoofed mammal. It belongs to the taxonomic family Equidae and is one of two extant subspecies of Equus ferus. The horse has evolved over the past 45 to 55 million years from a small multi-toed creature, Eohippus, into the large, single-toed animal of today.'
+    },
+    {
+        id: 10,
+        name: 'Koala',
+        image: Koala,
+        type: 'arboreal herbivorous marsupial',
+        details: 'The koala or, inaccurately, koala bear is an arboreal herbivorous marsupial native to Australia. It is the only extant representative of the family Phascolarctidae and its closest living relatives are the wombats.'
+    },
+    {
+        id: 11,
+        name: 'Lion',
+        image: Lion,
+        type: 'Panthera',
+        details: 'The lion is a large cat of the genus Panthera native to Africa and India. It has a muscular, broad-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane.'
+    },
+    {
+        id: 12,
+        name: 'Lobster',
+        image: Lobster,
+        type: 'marine crustaceans',
+        details: 'Lobsters are a family of large marine crustaceans. Lobsters have long bodies with muscular tails, and live in crevices or burrows on the sea floor. Three of their five pairs of legs have claws, including the first pair, which are usually much larger than the others.'
+    },
+    {
+        id: 13,
+        name: 'Polar Bear',
+        image: PolarBear,
+        type: 'hypercarnivorous',
+        details: 'The polar bear is a hypercarnivorous bear whose native range lies largely within the Arctic Circle, encompassing the Arctic Ocean, its surrounding seas and surrounding land masses. It is the largest extant bear species, as well as the largest extant land carnivore.'
+    },
+    {
+        id: 14,
+        name: 'Rabbit',
+        image: Rabbit,
+        type: 'Leporidae',
+        details: 'Rabbits, also known as bunnies or bunny rabbits, are small mammals in the family Leporidae of the order Lagomorpha. Oryctolagus cuniculus includes the European rabbit species and its descendants, the world\'s 305 breeds of domestic rabbit'
+    },
+    {
+        id: 15,
+        name: 'Reindeer',
+        image: Reindeer,
+        type: 'circumpolar distribution',
+        details: 'The reindeer, also known as the caribou in North America, is a species of deer with circumpolar distribution, native to Arctic, subarctic, tundra, boreal, and mountainous regions of range of caribou '
+    },
+    {
+        id: 16,
+        name: 'Rhinoceros',
+        image: Rhino,
+        type: 'odd-toed ungulates',
+        details: 'A rhinoceros, commonly abbreviated to rhino, is a member of any of the five extant species of odd-toed ungulates in the family Rhinocerotidae. Two of the extant species are native to Africa, and three to South and Southeast Asia'
+    },
+    {
+        id: 17,
+        name: 'Shark',
+        image: Shark,
+        type: 'elasmobranch fish',
+        details: 'Sharks are a group of elasmobranch fish characterized by a cartilaginous skeleton, five to seven gill slits on the sides of the head, and pectoral fins that are not fused to the head. Modern sharks are classified within the clade Selachimorpha and are the sister group to the rays'
+    },
+    {
+        id: 18,
+        name: 'Whale',
+        image: Whale,
+        type: 'aquatic placental marine mammals',
+        details: 'Whales are a widely distributed and diverse group of fully aquatic placental marine mammals. They are an informal grouping within the infraorder Cetacea, which usually excludes dolphins and porpoises. Whales, dolphins and porpoises belong to the order Cetartiodactyla, which consists of even-toed ungulates'
+    },
+    {
+        id: 19,
+        name: 'Zebra',
+        image: Zebra,
+        type: 'African equines',
+        details: 'Zebras are African equines with distinctive black-and-white striped coats. There are three living species: the Grévy\'s zebra, plains zebra, and the mountain zebra. Zebras share the genus Equus with horses and asses, the three groups being the only living members of the family Equidae'
+    },
+]
+
+
+export default animals;
